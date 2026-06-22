5b92c59c0d42f412dff9933215bde1202990fbb9
juniorbouani <juniorbouani@gmail.com>
2026-06-22 19:18:08 -0300

    first commit

---
+++ src/test/java/com/georges/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georges.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
