8da6bad016d968f8f34fa61dc6b45f16f43fefdf
juniorbouani <juniorbouani@gmail.com>
2026-06-30 20:04:28 -0300

    UserDto and Instantiation

--- src/main/java/com/georges/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/georges/workshopmongo/WorkshopmongoApplication.java
@@ -11,22 +11,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class WorkshopmongoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopmongoApplication.class, args);
-	}
-
-	@Bean
-	public MongoClient mongoClient() {
-		ConnectionString connectionString = new ConnectionString("mongodb://127.0.0.1:27017/workshop_mongo");
-		MongoClientSettings settings = MongoClientSettings.builder()
-				.applyConnectionString(connectionString)
-				.build();
-		return MongoClients.create(settings);
-	}
-
-	@Bean
-	public MongoTemplate mongoTemplate(MongoClient mongoClient) {
-		return new MongoTemplate(mongoClient, "workshop_mongo");
 	}
 }
