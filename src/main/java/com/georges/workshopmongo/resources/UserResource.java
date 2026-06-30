36dfdafb149cb7dbe9b51c179ce8baa0e1de020b
juniorbouani <juniorbouani@gmail.com>
2026-06-30 20:44:34 -0300

    Insercao de usuario com post

--- src/main/java/com/georges/workshopmongo/resources/UserResource.java
+++ src/main/java/com/georges/workshopmongo/resources/UserResource.java
@@ -6,7 +6,9 @@ import com.georges.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -32,6 +34,11 @@ public class UserResource {
         return ResponseEntity.ok().body(new UserDTO(obj));
     }
 
-
-
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+        User obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
 }
--- src/main/java/com/georges/workshopmongo/services/UserService.java
+++ src/main/java/com/georges/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.georges.workshopmongo.services;
 
 import com.georges.workshopmongo.domain.User;
+import com.georges.workshopmongo.dto.UserDTO;
 import com.georges.workshopmongo.repository.UserRepository;
 import com.georges.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
         User user = repo.findById(id)
                 .orElseThrow(() -> new ObjectNotFoundException("Obejeto não encontrado!"));
         return user;
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
