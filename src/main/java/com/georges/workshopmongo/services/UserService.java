4be7e35068621a00b53c93a3acefa52a96a23f76
juniorbouani <juniorbouani@gmail.com>
2026-06-30 20:51:39 -0300

    Incresed delete by id

--- src/main/java/com/georges/workshopmongo/services/UserService.java
+++ src/main/java/com/georges/workshopmongo/services/UserService.java
@@ -29,7 +29,13 @@ public class UserService {
         return repo.insert(obj);
     }
 
+
     public User fromDTO(UserDTO objDto) {
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    }
+
+    public void delete(String id) {
+        findById(id);
+        repo.deleteById(id);
     }
 }
